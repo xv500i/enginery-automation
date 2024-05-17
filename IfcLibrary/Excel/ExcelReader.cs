324a64355e58ed40e04802c2578155b5827c7500
Alex <xv500i@gmail.com>
2024-05-17 20:05:14 +0200

    read excel file

---
+++ IfcLibrary/Excel/ExcelReader.cs
@@ -0,0 +1,39 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfcLibrary.Excel
+{
+    public class ExcelReader
+    {
+        public List<List<string>> GetCells(string path)
+        {
+            var results = new List<List<string>>();
+            using (var stream = File.Open(path, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var result = reader.AsDataSet();
+
+                    var firstSheet = result.Tables[0];
+
+                    foreach (DataRow row in firstSheet.Rows) 
+                    {
+                        var rowAsList = new List<string>();
+                        foreach (DataColumn column in firstSheet.Columns)
+                        {
+                            rowAsList.Add(row[column]?.ToString() ?? string.Empty);
+                        }
+                        results.Add(rowAsList);
+                    }
+                }
+            }
+            return results;
+        }
+    }
+}
---
+++ IfcLibraryTests/ExcelReaderTests.cs
@@ -0,0 +1,18 @@
+﻿using IfcLibrary.Excel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace IfcLibraryTests
+{
+    [TestClass]
+    public class ExcelReaderTests
+    {
+        [TestMethod]
+        public void CanReadExcel()
+        {
+            var reader = new ExcelReader();
+            var cells = reader.GetCells(Path.Combine("TestData", "Libro1.xlsx"));
+            Assert.IsNotNull(cells);
+        }
+    }
+}
