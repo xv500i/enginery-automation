f9eb1a208d011b0322cbe18e0c1e12d61320ef0a
Alex <xv500i@gmail.com>
2024-06-08 20:42:29 +0200

    implement AddPropertySetWithPropertyAndValue as the customer expects

--- IfcLibrary/Ifc/IfcAdapter.cs
+++ IfcLibrary/Ifc/IfcAdapter.cs
@@ -1,5 +1,8 @@
 ﻿using IfcLibrary.Domain;
+using System;
+using System.Linq;
 using Xbim.Ifc;
+using Xbim.Ifc4.Interfaces;
 using Xbim.Ifc4.Kernel;
 using Xbim.Ifc4.MeasureResource;
 using Xbim.Ifc4.PropertyResource;
@@ -15,60 +18,118 @@ namespace IfcLibrary.Ifc
                 var transaction = model.BeginTransaction();
                 foreach(var update in  automatedChanges.UpdatePropertySetByValues)
                 {
-                    foreach(var propertySet in model.Instances.OfType<IfcPropertySet>())
-                    {
-                        // What if does not exist
-                        if(propertySet.Name == update.PropertySetName)
-                        {
-                            var property = propertySet.HasProperties.FirstOrDefault(x => x.Name == update.PropertyName);
-                            if (property != null) 
-                            {
-                                // TODO: implement all possible property types
-                                if (property is Xbim.Ifc4.PropertyResource.IfcPropertySingleValue v)
-                                {
-                                    var type = v.NominalValue.UnderlyingSystemType;
-                                    // TODO: respect type
-                                    if (type == typeof(string))
-                                    {
-                                        v.NominalValue = new IfcText(update.NewValue);
-                                    } else
-                                    {
-                                        v.NominalValue = new IfcLengthMeasure(update.NewValue);
-                                    }
-
-                                }
-                            }
-                            else
-                            {
-                                propertySet.HasProperties.Add(
-                                    model.Instances.New<IfcPropertySingleValue>(p =>
-                                    {
-                                        p.Name = update.PropertyName;
-                                        p.NominalValue = new IfcText(update.NewValue);
-                                    }));
-                            }
-                        }
-                    }
+                    ApplyUpdatePropertySetByValue(model, update);
                 }
 
                 foreach (var add in automatedChanges.AddPropertySetWithPropertyAndValues)
                 {
-                    foreach (var propertySet in model.Instances.OfType<IfcPropertySet>())
-                    {
-                        // What if does not exist
-                        if (propertySet.Name == add.NewPropertySetName)
-                        {
-                            propertySet.HasProperties.Add(model.Instances.New<IfcPropertySingleValue>(p =>
-                            {
-                                p.Name = add.NewPropertyName;
-                                // TODO: support types
-                                p.NominalValue = new IfcText(add.NewValue);
-                            }));
-                        }
-                    }
+                    ApplyAddPropertySetWithPropertyAndValue(model, add);
                 }
                 transaction.Commit();
                 model.SaveAs(patchedPath);
+            }
+        }
+
+        private static void ApplyAddPropertySetWithPropertyAndValue(IfcStore model, AddPropertySetWithPropertyAndValue add)
+        {
+            var allTargetObjects = model.Instances.OfType<IfcObject>()
+                .Where(x =>
+                {
+                    switch(x)
+                    {
+                        //case IIfcWall _:
+                        //case IIfcCableCarrierFitting _:
+                        //case IIfcCableCarrierSegment _:
+                        //case IIfcCableSegment _:
+                        //case IIfcProtectiveDevice _:
+                        //case IIfcElectricDistributionBoard _:
+                        //case IIfcEnergyConversionDevice _:
+                        //case IIfcSwitchingDevice _:
+                        //    return true;
+                        case IIfcDistributionPort _:
+                            // TODO: Da problemas
+                            return false;
+                        case IIfcObject _:
+                            return true;
+                    }
+                    
+                    return false;
+                });
+
+            foreach (var ifcObject in allTargetObjects)
+            {
+                var addPropertySet = ifcObject.PropertySets.FirstOrDefault(x => x.Name == add.NewPropertySetName);
+                if (addPropertySet == null)
+                {
+                    addPropertySet = model.Instances.New<IfcPropertySet>(p =>
+                    {
+                        p.Name = add.NewPropertySetName;
+                    });
+                    var pSetRel = model.Instances.New<IfcRelDefinesByProperties>(r =>
+                    {
+                        r.GlobalId = Guid.NewGuid();
+                        r.RelatingPropertyDefinition = addPropertySet;
+                    });
+                    pSetRel.RelatedObjects.Add(ifcObject);
+                }
+
+                var addProperty = addPropertySet.HasProperties.FirstOrDefault(x => x.Name == add.NewPropertyName);
+                if (addProperty == null)
+                {
+                    addProperty = model.Instances.New<IfcPropertySingleValue>(p =>
+                    {
+                        p.Name = add.NewPropertyName;
+                    });
+                    addPropertySet.HasProperties.Add(addProperty);
+                }
+                switch (addProperty)
+                {
+                    case IfcPropertySingleValue ifcPropertySingleValue:
+                        ifcPropertySingleValue.NominalValue = new IfcText(add.NewValue);
+                        break;
+                    case object o:
+                        var a = 1;
+                        break;
+                }
+            }
+        }
+
+        private static void ApplyUpdatePropertySetByValue(IfcStore model, UpdatePropertySetByValue update)
+        {
+            foreach (var propertySet in model.Instances.OfType<IfcPropertySet>())
+            {
+                // What if does not exist
+                if (propertySet.Name == update.PropertySetName)
+                {
+                    var property = propertySet.HasProperties.FirstOrDefault(x => x.Name == update.PropertyName);
+                    if (property != null)
+                    {
+                        // TODO: implement all possible property types
+                        if (property is IfcPropertySingleValue v)
+                        {
+                            var type = v.NominalValue.UnderlyingSystemType;
+                            // TODO: respect type
+                            if (type == typeof(string))
+                            {
+                                v.NominalValue = new IfcText(update.NewValue);
+                            }
+                            else
+                            {
+                                v.NominalValue = new IfcLengthMeasure(update.NewValue);
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        propertySet.HasProperties.Add(
+                            model.Instances.New<IfcPropertySingleValue>(p =>
+                            {
+                                p.Name = update.PropertyName;
+                                p.NominalValue = new IfcText(update.NewValue);
+                            }));
+                    }
+                }
             }
         }
     }
--- IfcLibraryTests/IfcReaderTests.cs
+++ IfcLibraryTests/IfcReaderTests.cs
@@ -77,6 +77,32 @@ namespace IfcLibraryTests
         }
 
         [TestMethod]
+        public void AddPropertySetWithPropertyAndValue_PropertySetDoesNotExist_CreatesThePropertyInThePropertySet()
+        {
+            var reader = new IfcAdapter();
+
+            var automatedChanges = new AutomatedChanges();
+            automatedChanges.AddPropertySetWithPropertyAndValues.Add(
+                new AddPropertySetWithPropertyAndValue
+                {
+                    NewPropertySetName = "1239788974123",
+                    NewPropertyName = "7640923409",
+                    NewValue = "212349090123",
+                });
+
+            reader.PatchFile(
+                Path.Combine("TestData", "simple.ifc"),
+                Path.Combine("TestData", "simple_patched.ifc"),
+                automatedChanges);
+
+            var contents = File.ReadAllLines(Path.Combine("TestData", "simple_patched.ifc"));
+
+            Assert.IsNotNull(contents.FirstOrDefault(x => x.Contains("IFCTEXT('212349090123')")));
+            Assert.IsNotNull(contents.FirstOrDefault(x => x.Contains("7640923409")));
+            Assert.IsNotNull(contents.FirstOrDefault(x => x.Contains("1239788974123")));
+        }
+
+        [TestMethod]
         public void CanPatchWithBigFilePropertyGroup()
         {
             var reader = new IfcAdapter();
